0dda49c1169d34f788454fc70aff9a44ab86530a
belyuchenko.olga <belyuchenko.olga@wb.ru>
2025-12-15 11:10:08 +0300

    int tasks

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import "github.com/helga228/tasks/int"
+
+func main() {
+	// Базовые операции
+	int_task.SimpleCalculations()
+	// Переполнение и типы
+	int_task.Overflow()
+	// Побитовые операции
+	int_task.Bitwise()
+	// Преобразование типов
+	int_task.TypeConversion()
+	//
+	int_task.ProcessNumberDigits()
+	//
+	int_task.RoundAndCompareFloat()
+}
